1216275ef9c378a386dc7e29a7e1da2dd9b497d3
ragrog <ragrog216@outlook.jp>
2016-11-04 16:01:01 +0900

    update

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -24,7 +24,9 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'Home', 'action' => 'index', 'home'));
+	Router::connect('/view', array('controller' => 'Home', 'action' => 'view'));
+	Router::connect('/likes', array('controller' => 'Home', 'action' => 'likes'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
---
+++ app/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class HomeController extends AppController {
+	public function index() {
+
+	}
+	public function likes() {
+
+	}
+	public function view() {
+
+	}
+}
