36c12ad310ff04ef5d194ed9a082036ded8ef97c
ragrog <ragrog216@outlook.jp>
2016-11-04 15:43:19 +0900

    home/likesとhome/viewのルーティングを修正

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,6 +25,8 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'Home', 'action' => 'index', 'home'));
+	Router::connect('/view', array('controller' => 'Home', 'action' => 'view'));
+	Router::connect('/likes', array('controller' => 'Home', 'action' => 'likes'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
