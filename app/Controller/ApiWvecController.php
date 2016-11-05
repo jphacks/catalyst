f69467890ab1429937e5339dd184ae6af5649cec
ragrog <ragrog216@outlook.jp>
2016-11-05 15:15:21 +0900

    検索履歴を保存する処理を組み込んだ

--- app/Controller/ApiWvecController.php
+++ app/Controller/ApiWvecController.php
@@ -4,6 +4,13 @@ App::uses('AppController', 'Controller');
 
 class ApiWvecController extends AppController {
 	public $components = array('RequestHandler');
+	public $uses = array('SearchKey');
+
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('index');
+	}
+
 	public function index() {
 		$isError = false;
 		$response = null;
@@ -28,8 +35,13 @@ class ApiWvecController extends AppController {
 			curl_setopt_array($ch, $options);
 			$response =curl_exec($ch);
 			curl_close($ch);
+			if (!empty($this->Auth->user())) {
+				$this->SearchKey->saveData(array(
+					'user_id' => $this->Auth->user('id'),
+					'word' => $word
+				));
+			}
 		}
-
 		$this->set("json", $response);
 	}
 }
